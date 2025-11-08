6f48dc8452df10ff1ed2669b719f574d5f6565c5
Abhirup Sengupta <217376601+abhirup-2005@users.noreply.github.com>
2025-11-09 03:27:36 +0530

    Change Theme button position and colour
    -changed theme button position from fotter to header
    -added toggle colour changed
    -resized the button

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
-// === Common Script for All Pages ===
-
 document.addEventListener("DOMContentLoaded", () => {
   const themeBtn = document.getElementById("themeToggle");
-  if (!themeBtn) return; // if button not found on page
+  if (!themeBtn) return;
 
   themeBtn.addEventListener("click", () => {
     document.body.classList.toggle("light-mode");
     themeBtn.textContent = document.body.classList.contains("light-mode")
-      ? "🌙 Dark Mode"
-      : "☀️ Light Mode";
+      ? "🌙Dark"
+      : "☀️Light";
   });
 });
